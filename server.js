95a7f5c91ba9e3012f83c823dbc8da68d8fecac6
Angelo <hx256561@gmail.com>
2023-05-17 14:04:24 +0800

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const mongoose=require('mongoose');
 const app=express();
 
 
@@ -7,7 +8,14 @@ app.get('/',(req,res)=>{
     res.send('Hello Node API');
 });
 
-
-app.listen(3000, ()=>{
-    console.log('Node API is running on port 3000');
-});
+// mongoose connection. Replaced the <password> with real password. Replaced ? with collection name
+// connect to the MongoDB first and then listen
+mongoose.connect('mongodb+srv://herman256561:d4fe0e88@angeloapi.zmxyy67.mongodb.net/Node-APIretryWrites=true&w=majority')
+.then(()=>{
+    console.log("connected to MongoDB");
+    app.listen(3000, ()=>{
+        console.log('Node API is running on port 3000');
+    });
+}).catch((error)=>{
+    console.log(error);
+})
